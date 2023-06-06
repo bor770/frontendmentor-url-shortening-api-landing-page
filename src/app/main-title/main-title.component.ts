87c9b07bc84714afa028378894ad58093bd729b1
Baruch Giverts <bor770@gmail.com>
2023-06-06 11:42:13 +0300

    styled mobile menu

--- src/app/main-title/main-title.component.ts
+++ src/app/main-title/main-title.component.ts
@@ -6,7 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [CommonModule, MatButtonModule],
   selector: 'app-main-title',
   standalone: true,
-  styleUrls: ['./main-title.component.css'],
+  styleUrls: ['./main-title.component.css', `../shared/styles/styles.css`],
   templateUrl: './main-title.component.html',
 })
 export class MainTitleComponent {}
