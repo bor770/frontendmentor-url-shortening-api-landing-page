391fb3709ae4f482364ba9d5a65bc20961568a7c
Baruch Giverts <bor770@gmail.com>
2023-06-07 13:08:26 +0300

    header desktop html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,27 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Observable, map } from 'rxjs';
 
 import { MenuComponent } from './menu/menu.component';
 
 @Component({
-  imports: [CommonModule, MatToolbarModule, MenuComponent],
+  imports: [CommonModule, MatButtonModule, MatToolbarModule, MenuComponent],
   selector: 'app-header',
   standalone: true,
   styleUrls: ['./header.component.css'],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  isXSmall$: Observable<boolean>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.isXSmall$ = this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .pipe(map((result) => result.matches));
+  }
+}
