5e4cbfe0df4e91a9395bbdd3d4b0176586ffc019
Baruch Giverts <bor770@gmail.com>
2023-06-07 15:00:40 +0300

    in middle of styling shorten

--- src/app/shorten/shorten.component.ts
+++ src/app/shorten/shorten.component.ts
@@ -1,4 +1,5 @@
 import { ClipboardModule } from '@angular/cdk/clipboard';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
@@ -12,7 +13,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 import { Links } from './shorten.model';
 import * as fromRoot from '../store/root.reducer';
@@ -39,11 +40,20 @@ export class ShortenComponent implements OnInit {
   copied$: Observable<number>;
   links$: Observable<Links>;
 
-  constructor(private store: Store) {}
+  isMinLarge$: Observable<boolean>;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private store: Store
+  ) {}
 
   ngOnInit(): void {
     this.copied$ = this.store.select(fromRoot.selectShortenCopied);
     this.links$ = this.store.select(fromRoot.selectShortenLinks);
+
+    this.isMinLarge$ = this.breakpointObserver
+      .observe([Breakpoints.Large, Breakpoints.XLarge])
+      .pipe(map((result) => result.matches));
 
     this.form = new FormGroup({
       originalLink: new FormControl(null, Validators.required),
