4f492e8e5faec40dc825443239814c48a0a15dcc
Baruch Giverts <bor770@gmail.com>
2023-06-07 12:40:19 +0300

    styled mobile

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,35 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { DomSanitizer } from '@angular/platform-browser';
 
 import { links, socialIcons } from './links';
 
 @Component({
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule],
   selector: 'app-footer',
   standalone: true,
   styleUrls: ['./footer.component.css'],
   templateUrl: './footer.component.html',
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
   links = links;
   socialIcons = socialIcons;
-
-  constructor(
-    private iconRegistry: MatIconRegistry,
-    private sanitizer: DomSanitizer
-  ) {}
-
-  ngOnInit(): void {
-    for (const icon of this.socialIcons) {
-      this.iconRegistry.addSvgIcon(
-        icon,
-        this.sanitizer.bypassSecurityTrustResourceUrl(
-          `../../assets/images/icon-${icon}.svg`
-        )
-      );
-    }
-  }
 }
