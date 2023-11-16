a5804a3c4254ee73d996f5f047dee3a7769a2664
Baruch Giverts <bor770@gmail.com>
2023-11-16 15:14:21 +0200

    mobile more section

---
+++ src/app/main/get-started-button/get-started-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-get-started-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './get-started-button.component.html',
+  styleUrl: './get-started-button.component.css'
+})
+export class GetStartedButtonComponent {
+
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { LetDirective } from '@ngrx/component';
+
+import { BaseComponent } from '../shared/base/base.component';
+import { GetStartedButtonComponent } from './get-started-button/get-started-button.component';
+import { Width } from '../shared/layout/layout.model';
 
 @Component({
+  imports: [CommonModule, LetDirective, GetStartedButtonComponent],
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: [
+    './styles/main.component.css',
+    `./styles/mobile.main.component.css`,
+    `./styles/desktop.main.component.css`,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
 })
-export class MainComponent {
+export class MainComponent extends BaseComponent {
+  imgSrcCloud(width: Width) {
+    return `url(../../assets/images/cloud-${width}.svg)`;
+  }
 
+  imgSrcWorking(width: Width) {
+    return `../../assets/images/working-${width}.svg`;
+  }
 }
